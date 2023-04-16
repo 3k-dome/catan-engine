bb584e3d8310e4c6fa20eeeb94d39fd8aba92e55
3k-dome <doenerfresser3k@gmail.com>
2023-04-16 19:30:39 +0200

    [#2]: add collection of all tiles

---
+++ catan-lib/Sets/HexTileSet.cs
@@ -0,0 +1,37 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces;
+using CatanLib.Parts;
+
+namespace CatanLib.Sets
+{
+    public static class HexTileSet
+    {
+        public static readonly IEnumerable<IHexTile> Tiles = new List<HexTile>()
+        {
+            new() { Terrain = TerrainType.Desert },
+
+            new() { Terrain = TerrainType.Mountain },
+            new() { Terrain = TerrainType.Mountain },
+            new() { Terrain = TerrainType.Mountain },
+
+            new() { Terrain = TerrainType.Hill },
+            new() { Terrain = TerrainType.Hill },
+            new() { Terrain = TerrainType.Hill },
+
+            new() { Terrain = TerrainType.Field },
+            new() { Terrain = TerrainType.Field },
+            new() { Terrain = TerrainType.Field },
+            new() { Terrain = TerrainType.Field },
+
+            new() { Terrain = TerrainType.Forest },
+            new() { Terrain = TerrainType.Forest },
+            new() { Terrain = TerrainType.Forest },
+            new() { Terrain = TerrainType.Forest },
+
+            new() { Terrain = TerrainType.Pasture },
+            new() { Terrain = TerrainType.Pasture },
+            new() { Terrain = TerrainType.Pasture },
+            new() { Terrain = TerrainType.Pasture },
+        };
+    }
+}
