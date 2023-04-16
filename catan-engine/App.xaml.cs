8ed46418d878436ffb0a56da5465e8a2e87f0792
3k-dome <doenerfresser3k@gmail.com>
2023-04-16 14:33:30 +0200

    Projektdateien hinzufügen.

---
+++ catan-engine/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CatanEngine
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs eventArgs)
+        {
+            base.OnStartup(eventArgs);
+        }
+    }
+}
