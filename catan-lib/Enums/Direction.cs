30eea7356edd54b274aecca97c7a9c15cfd6fc55
3k-dome <doenerfresser3k@gmail.com>
2023-04-17 23:56:01 +0200

    [#4]: add cardinal directions and corresponding offset coordniates

---
+++ catan-lib/Enums/Direction.cs
@@ -0,0 +1,28 @@
+﻿using CatanLib.Interfaces;
+using CatanLib.Parts;
+
+namespace CatanLib.Enums
+{
+    public enum Direction
+    {
+        West,
+        NorthWest,
+        NorthEast,
+        East,
+        SouthEast,
+        SouthWest,
+    }
+
+    public static class DirectionCoordinates
+    {
+        public static readonly Dictionary<Direction, IHexCoordinate> Coordinates = new()
+        {
+            { Direction.West, new HexCoordinate(1, 0, -1) },
+            { Direction.NorthWest, new HexCoordinate(1, -1, 0) },
+            { Direction.NorthEast, new HexCoordinate(0, -1, 1) },
+            { Direction.East, new HexCoordinate(-1, 0, 1) },
+            { Direction.SouthEast, new HexCoordinate(-1, 1, 0) },
+            { Direction.SouthWest, new HexCoordinate(0, 1, -1) },
+        };
+    }
+}
