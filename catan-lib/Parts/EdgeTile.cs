1311c64d606403f1f22cf6ad28eadcbab74cdeeb
3k-dome <doenerfresser3k@gmail.com>
2023-04-18 13:13:58 +0200

    [#2, #6]: add edge tiles and borders

---
+++ catan-lib/Parts/EdgeTile.cs
@@ -0,0 +1,57 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces;
+
+namespace CatanLib.Parts
+{
+    public class EdgeTile : IEdgeTile
+    {
+        public bool SeaTrade { get; init; }
+
+        public ResourceType? ResourceTrade { get; init; }
+
+        private IHexCoordinate? coordinate;
+        public IHexCoordinate Coordinate
+        {
+            get => coordinate ?? throw new NullReferenceException();
+            set => coordinate ??= value;
+        }
+
+        private float[]? tradeEncoding;
+        public float[] TradeEncoding
+        {
+            get
+            {
+                if (tradeEncoding != null)
+                {
+                    return tradeEncoding;
+                }
+
+                tradeEncoding = new float[Enum.GetValues<ResourceType>().Length];
+                if (ResourceTrade != null)
+                {
+                    tradeEncoding[(int)ResourceTrade] = 1;
+                }
+                return tradeEncoding;
+            }
+        }
+
+        public IEnumerable<string> ToExplainedVector()
+        {
+            // not backed by a lazy property since this is just a utility method
+            int index = 0;
+            string[] descriptions = new string[Enum.GetValues<ResourceType>().Length];
+            foreach (ResourceType terrainType in Enum.GetValues<ResourceType>())
+            {
+                descriptions[index] = $"Trades{Enum.GetName(terrainType)}";
+                index++;
+            }
+
+            return descriptions;
+        }
+
+        public IEnumerable<float> ToVector()
+        {
+            return TradeEncoding;
+        }
+    }
+}
---
+++ catan-lib/Sets/EdgeTileSet.cs
@@ -0,0 +1,49 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces;
+using CatanLib.Parts;
+
+namespace CatanLib.Sets
+{
+    public static class EdgeTileSet
+    {
+        public static readonly IEnumerable<IEnumerable<IEdgeTile>> Tiles = new List<List<EdgeTile>>()
+        {
+            new() // border 1:2
+            {
+                new() { ResourceTrade = ResourceType.Sheep, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = true },
+            },
+            new() // border 2:3
+            {
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = ResourceType.Ore, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = false },
+            },
+            new() // border 3:4
+            {
+                new() { ResourceTrade = ResourceType.Wheat, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = true },
+            },
+            new() // border 4:5
+            {
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = ResourceType.Wood, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = false },
+            },
+            new() // border 5:6
+            {
+                new() { ResourceTrade = ResourceType.Brick, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = true },
+            },
+            new() // border 6:1
+            {
+                new() { ResourceTrade = null, SeaTrade = false },
+                new() { ResourceTrade = null, SeaTrade = true },
+                new() { ResourceTrade = null, SeaTrade = false },
+            },
+        };
+    }
+}
