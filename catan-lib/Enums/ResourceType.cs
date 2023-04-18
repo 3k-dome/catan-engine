24bebf8b73961a64707ed2169e9ce25c63983a44
3k-dome <doenerfresser3k@gmail.com>
2023-04-18 12:32:56 +0200

    [#2]: add resouces and lookup table

---
+++ catan-lib/Enums/ResourceType.cs
@@ -0,0 +1,24 @@
+﻿namespace CatanLib.Enums
+{
+    public enum ResourceType
+    {
+        Sheep,
+        Wheat,
+        Ore,
+        Brick,
+        Wood,
+        None
+    }
+
+    public static class TerrainResources
+    {
+        public static readonly Dictionary<TerrainType, ResourceType> Resources = new()
+        {
+            { TerrainType.Pasture, ResourceType.Sheep },
+            { TerrainType.Field, ResourceType.Wheat },
+            { TerrainType.Mountain, ResourceType.Ore },
+            { TerrainType.Hill, ResourceType.Brick },
+            { TerrainType.Forest, ResourceType.Wood },
+        };
+    }
+}
