127e9b9205830eee1c5bb99dae88aae972c18c32
3k-dome <doenerfresser3k@gmail.com>
2023-04-19 22:18:58 +0200

    [#4]: add abstractions

---
+++ hexagon-lib/Abstractions/HexagonalCoordinate.cs
@@ -0,0 +1,26 @@
+﻿namespace HexagonLib.Abstractions
+{
+    public abstract class HexagonalCoordinate : IHexagonalCoordinate
+    {
+        public int X { get; protected set; }
+
+        public int Y { get; protected set; }
+
+        public int Z { get; protected set; }
+
+        public HexagonalCoordinate(int x, int y, int z)
+        {
+            (X, Y, Z) = (x, y, z);
+        }
+
+        public override bool Equals(object? other)
+        {
+            return other != null && other is HexagonalCoordinate && GetHashCode() == other.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+    }
+}
---
+++ hexagon-lib/Abstractions/IHexagonalCoordinate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+namespace HexagonLib.Abstractions
+{
+    public interface IHexagonalCoordinate
+    {
+        public int X { get; }
+        public int Y { get; }
+        public int Z { get; }
+    }
+}
