efe91097f6b4fe6edc190c4c4b56c5ebb55a9647
3k-dome <doenerfresser3k@gmail.com>
2023-04-24 23:51:51 +0200

    [#7]: add settlements

---
+++ catan-lib/Interfaces/ISettlement.cs
@@ -0,0 +1,13 @@
+﻿using CatanLib.Enums;
+using HexagonLib;
+
+namespace CatanLib.Interfaces
+{
+    public interface ISettlement : IVectorizable
+    {
+        VertexCoordinate Vertex { get; set; }
+        bool IsSettlement { get; }
+        bool IsCity { get; }
+        PlayerNumber? Belongs { get; }
+    }
+}
---
+++ catan-lib/Parts/Settlement.cs
@@ -0,0 +1,40 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces;
+using HexagonLib;
+
+namespace CatanLib.Parts
+{
+    public class Settlement : ISettlement
+    {
+        private VertexCoordinate? vertex;
+        public VertexCoordinate Vertex
+        {
+            get => vertex ?? throw new NullReferenceException();
+            set => vertex ??= value;
+        }
+
+        public bool IsSettlement { get; private set; }
+
+        public bool IsCity { get; private set; }
+
+        public PlayerNumber? Belongs { get; private set; }
+
+        public IEnumerable<float> ToVector()
+        {
+
+            float[] playerEncoding = new float[Enum.GetValues<PlayerNumber>().Length];
+            if (Belongs != null)
+            {
+                playerEncoding[(int)Belongs] = 1;
+            }
+
+            float[] buildingEncoding = new float[] { IsSettlement ? 1 : 0, IsCity ? 1 : 0 };
+            return Enumerable.Concat(playerEncoding, buildingEncoding);
+        }
+
+        public IEnumerable<string> ToExplainedVector()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
