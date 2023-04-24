3d414de7729fe987f6c9988acebabaeb9230a952
3k-dome <doenerfresser3k@gmail.com>
2023-04-24 23:51:27 +0200

    [#8]: add roads

---
+++ catan-lib/Interfaces/IRoad.cs
@@ -0,0 +1,12 @@
+﻿using CatanLib.Enums;
+using HexagonLib;
+
+namespace CatanLib.Interfaces
+{
+    public interface IRoad : IVectorizable
+    {
+        EdgeCoordinate Edge { get; set; }
+        bool IsRoad { get; }
+        PlayerNumber? Belongs { get; }
+    }
+}
---
+++ catan-lib/Parts/Road.cs
@@ -0,0 +1,38 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces;
+using HexagonLib;
+
+namespace CatanLib.Parts
+{
+    public class Road : IRoad
+    {
+        private EdgeCoordinate? edge;
+        public EdgeCoordinate Edge
+        {
+            get => edge ?? throw new NullReferenceException();
+            set => edge ??= value;
+        }
+
+        public bool IsRoad { get; private set; }
+
+        public PlayerNumber? Belongs { get; private set; }
+
+        public IEnumerable<float> ToVector()
+        {
+
+            float[] playerEncoding = new float[Enum.GetValues<PlayerNumber>().Length];
+            if (Belongs != null)
+            {
+                playerEncoding[(int)Belongs] = 1;
+            }
+
+            float[] buildingEncoding = new float[] { IsRoad ? 1 : 0 };
+            return Enumerable.Concat(playerEncoding, buildingEncoding);
+        }
+
+        public IEnumerable<string> ToExplainedVector()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
