c49b87b405cb207ce20c2759ae1a64ea14ebde90
dominik.buschold <dominik.buschold@uni-jena.de>
2023-04-27 21:23:05 +0200

    [#8]: small update

--- catan-lib/Parts/Road.cs
+++ catan-lib/Parts/Road.cs
@@ -7,19 +7,19 @@ namespace CatanLib.Parts
     public class Road : IRoad
     {
         private EdgeCoordinate? edge;
-        public EdgeCoordinate Edge
-        {
+        public EdgeCoordinate Edge {
             get => edge ?? throw new NullReferenceException();
             set => edge ??= value;
         }
 
         public bool IsRoad { get; private set; }
-
         public PlayerNumber? Belongs { get; private set; }
+
+        private readonly IEnumerable<ResourceType> costs = new[] { ResourceType.Wood, ResourceType.Brick };
+        public IEnumerable<ResourceType> Costs => costs;
 
         public IEnumerable<float> ToVector()
         {
-
             float[] playerEncoding = new float[Enum.GetValues<PlayerNumber>().Length];
             if (Belongs != null)
             {
@@ -33,6 +33,33 @@ namespace CatanLib.Parts
         public IEnumerable<string> ToExplainedVector()
         {
             throw new NotImplementedException();
+        }
+
+        public void ActionPlace()
+        {
+            // this should; each game element that has actions
+            // should have access to the game itself, i.e. a container
+            // for the boad and players
+            // actions could then be performed from this container
+            throw new NotImplementedException();
+        }
+
+        public bool CanPlace()
+        {
+            // see action
+            // player needs enought resources & a spare road object
+            // no road was build here yet
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Action> GetActions()
+        {
+            return new Action[] { ActionPlace };
+        }
+
+        public IEnumerable<Func<bool>> CanExecuteActions()
+        {
+            return new Func<bool>[] { CanPlace };
         }
     }
 }
