166c7d9d6b56c5bd1e291666e7a59ee93c1d49a5
3k-dome <doenerfresser3k@gmail.com>
2023-05-01 21:42:16 +0200

    [#7]: implement new interface

--- catan-lib/Parts/Settlement.cs
+++ catan-lib/Parts/Settlement.cs
@@ -1,5 +1,6 @@
 ﻿using CatanLib.Enums;
 using CatanLib.Interfaces;
+using CatanLib.Interfaces.Components;
 using HexagonLib;
 
 namespace CatanLib.Parts
@@ -7,27 +8,23 @@ namespace CatanLib.Parts
     public class Settlement : ISettlement
     {
         private VertexCoordinate? vertex;
-        public VertexCoordinate Vertex {
+        public VertexCoordinate Vertex
+        {
             get => vertex ?? throw new NullReferenceException();
             set => vertex ??= value;
         }
 
         public bool IsSettlement { get; private set; }
-
         public bool IsCity { get; private set; }
-
-        public PlayerNumber? Belongs { get; private set; }
-
-        private readonly IEnumerable<ResourceType> costs = new[]
+        public IPlayer? Belongs { get; private set; }
+        public IEnumerable<ResourceType> Costs { get; } = new[]
         {
             ResourceType.Wood,
             ResourceType.Brick,
             ResourceType.Wheat,
             ResourceType.Sheep
         };
-        public IEnumerable<ResourceType> Costs => costs;
-
-        private readonly IEnumerable<ResourceType> upgradeCosts = new[]
+        public IEnumerable<ResourceType> UpgradeCosts { get; } = new[]
         {
             ResourceType.Wheat,
             ResourceType.Wheat,
@@ -35,22 +32,60 @@ namespace CatanLib.Parts
             ResourceType.Ore,
             ResourceType.Ore
         };
-        public IEnumerable<ResourceType> UpgradeCosts => upgradeCosts;
 
-        public IEnumerable<float> ToVector()
+        public void Play<TSettlement, TRoad, TDice>(Catan<TSettlement, TRoad, TDice> catan)
+        where TSettlement : ISettlement, new()
+        where TRoad : IRoad, new()
+        where TDice : IDice, new()
         {
-
-            float[] playerEncoding = new float[Enum.GetValues<PlayerNumber>().Length];
-            if (Belongs != null)
-            {
-                playerEncoding[(int)Belongs] = 1;
-            }
-
-            float[] buildingEncoding = new float[] { IsSettlement ? 1 : 0, IsCity ? 1 : 0 };
-            return Enumerable.Concat(playerEncoding, buildingEncoding);
+            catan.CurrentPlayer.UseResources(Costs);
+            Belongs = catan.CurrentPlayer;
+            (IsSettlement, IsCity) = (true, false);
         }
 
-        public IEnumerable<string> ToExplainedVector()
+        public bool CanPlay<TSettlement, TRoad, TDice>(Catan<TSettlement, TRoad, TDice> catan)
+        where TSettlement : ISettlement, new()
+        where TRoad : IRoad, new()
+        where TDice : IDice, new()
+        {
+            bool hasOwner = Belongs != null;
+            bool hasResources = catan.CurrentPlayer.HasResources(Costs);
+            bool placementRule = Vertex.Neighbors()
+                .Select(vertex => catan.Board[vertex])
+                .All(settlement => !settlement.IsSettlement && !settlement.IsCity);
+            return !hasOwner && hasResources && placementRule && !IsSettlement && !IsCity;
+        }
+
+        public void Upgrade<TSettlement, TRoad, TDice>(Catan<TSettlement, TRoad, TDice> catan)
+        where TSettlement : ISettlement, new()
+        where TRoad : IRoad, new()
+        where TDice : IDice, new()
+        {
+            catan.CurrentPlayer.UseResources(UpgradeCosts);
+            (IsSettlement, IsCity) = (false, true);
+        }
+
+        public bool CanUpgrade<TSettlement, TRoad, TDice>(Catan<TSettlement, TRoad, TDice> catan)
+        where TSettlement : ISettlement, new()
+        where TRoad : IRoad, new()
+        where TDice : IDice, new()
+        {
+            bool isOwner = Belongs == catan.CurrentPlayer;
+            bool hasResources = catan.CurrentPlayer.HasResources(UpgradeCosts);
+            return hasResources && isOwner && IsSettlement && !IsCity;
+        }
+
+        public IEnumerable<Action> GetActions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Func<bool>> CanExecuteActions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<float> ToVector()
         {
             throw new NotImplementedException();
         }
