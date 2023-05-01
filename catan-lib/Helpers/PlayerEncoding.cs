e7614e0941b9649ed1867e2b3e18d57e51606ef3
3k-dome <doenerfresser3k@gmail.com>
2023-05-01 21:55:33 +0200

    [#11]: add helper to order players

---
+++ catan-lib/Helpers/PlayerEncoding.cs
@@ -0,0 +1,16 @@
+﻿using CatanLib.Interfaces.Components;
+
+namespace CatanLib.Helpers
+{
+    public static class PlayerEncoding
+    {
+        public static IEnumerable<IPlayer> Order(IEnumerable<IPlayer> players, IPlayer current)
+        {
+            IEnumerable<IPlayer> opponets = players.Where(player => player.Number != current.Number)
+                .OrderByDescending(player => player.Number);
+
+            opponets = opponets.Append(current);
+            return opponets.Reverse();
+        }
+    }
+}
