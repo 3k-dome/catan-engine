e40e0fb2da21278dd57016dd46d748b519f67ae1
3k-dome <doenerfresser3k@gmail.com>
2023-05-03 22:07:08 +0200

    [#1]: update action vector for dependency injection

--- catan-lib/Interfaces/Interaction/IVectorizableActions.cs
+++ catan-lib/Interfaces/Interaction/IVectorizableActions.cs
@@ -1,6 +1,16 @@
-﻿namespace CatanLib.Interfaces;
+﻿using CatanLib.Interfaces.Components;
+using CatanLib.Parts;
+
+namespace CatanLib.Interfaces.Interaction;
 public interface IVectorizableActions
 {
-    IEnumerable<Action> GetActions();
-    IEnumerable<Func<bool>> CanExecuteActions();
+    IEnumerable<Action<Catan<TSettlement, TRoad, TDice>>> GetActions<TSettlement, TRoad, TDice>()
+    where TSettlement : ISettlement, new()
+    where TRoad : IRoad, new()
+    where TDice : IDice, new();
+
+    IEnumerable<Func<Catan<TSettlement, TRoad, TDice>, bool>> CanExecuteActions<TSettlement, TRoad, TDice>()
+    where TSettlement : ISettlement, new()
+    where TRoad : IRoad, new()
+    where TDice : IDice, new();
 }
