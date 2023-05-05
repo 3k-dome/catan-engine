b76b7609774735b075129022996c093147f39277
3k-dome <doenerfresser3k@gmail.com>
2023-05-05 23:12:57 +0200

    [#1]: component state also relies on board state

--- catan-lib/Interfaces/Interaction/IVectorizableComponent.cs
+++ catan-lib/Interfaces/Interaction/IVectorizableComponent.cs
@@ -1,8 +1,18 @@
-﻿namespace CatanLib.Interfaces
+﻿using CatanLib.Interfaces.Components;
+using CatanLib.Parts;
+
+namespace CatanLib.Interfaces.Interaction
 {
     public interface IVectorizableComponent
     {
-        public IEnumerable<float> ToVector();
-        public IEnumerable<string> ToExplainedVector() => throw new NotImplementedException();
+        public IEnumerable<float> ToVector<TSettlement, TRoad, TDice>(Catan<TSettlement, TRoad, TDice> catan)
+        where TSettlement : ISettlement, new()
+        where TRoad : IRoad, new()
+        where TDice : IDice, new();
+
+        public IEnumerable<string> ToExplainedVector()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
