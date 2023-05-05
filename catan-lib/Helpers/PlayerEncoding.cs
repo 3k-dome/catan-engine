9aa30e1d21a6aa45cff9eb5c6e11799d5d199916
3k-dome <doenerfresser3k@gmail.com>
2023-05-05 23:13:50 +0200

    [#11]: add suitable encoding

--- catan-lib/Helpers/PlayerEncoding.cs
+++ catan-lib/Helpers/PlayerEncoding.cs
@@ -1,4 +1,5 @@
-﻿using CatanLib.Interfaces.Components;
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces.Components;
 
 namespace CatanLib.Helpers
 {
@@ -11,6 +12,27 @@ namespace CatanLib.Helpers
 
             opponets = opponets.Append(current);
             return opponets.Reverse();
+        }
+
+        public static IEnumerable<float> Encode(IPlayer current, IPlayer? encode)
+        {
+            IEnumerable<PlayerNumber> playerNumers = Enum.GetValues<PlayerNumber>()
+                .Except(new[] { current.Number })
+                .OrderByDescending(number => number)
+                .Append(current.Number)
+                .Reverse();
+
+            float[] encoding = new float[playerNumers.Count()];
+
+            if (encode != null)
+            {
+                int index = playerNumers
+                    .TakeWhile(number => number != encode.Number)
+                    .Count();
+                encoding[index] = 1;
+            }
+
+            return encoding;
         }
     }
 }
