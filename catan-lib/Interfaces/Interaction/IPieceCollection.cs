7005265820c491fcb07510b6bdfc18767ebda8b3
3k-dome <doenerfresser3k@gmail.com>
2023-05-07 20:23:22 +0200

    [#13]: add new collection interfaces

---
+++ catan-lib/Interfaces/Interaction/IPieceCollection.cs
@@ -0,0 +1,33 @@
+﻿using CatanLib.Enums;
+
+namespace CatanLib.Interfaces.Interaction
+{
+    public interface IPieceCollection
+    {
+        Dictionary<PieceType, int> Pieces { get; }
+
+        void PlacePiece(PieceType piece)
+        {
+            if (piece == PieceType.None) { return; }
+
+            if (Pieces[piece] <= 0)
+            {
+                throw new Exception("Player can't spend any more of this Resource!");
+            }
+
+            Pieces[piece]--;
+        }
+
+        void TakePiece(PieceType piece)
+        {
+            if (piece == PieceType.None) { return; }
+
+            Pieces[piece]++;
+        }
+
+        bool HasPiece(PieceType piece)
+        {
+            return piece == PieceType.None || Pieces[piece] > 0;
+        }
+    }
+}
