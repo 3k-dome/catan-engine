0bd3536a0930475da3819308693c1790b217fa79
3k-dome <doenerfresser3k@gmail.com>
2023-05-14 21:42:29 +0200

    [#16]: add longest road calc

--- catan-lib/Helpers/TraverseRoads.cs
+++ catan-lib/Helpers/TraverseRoads.cs
@@ -6,44 +6,50 @@ namespace CatanLib.Helpers
 {
     public static class TraverseRoads
     {
-        public static bool IsValid(Board board, VertexCoordinate vertex, IPlayer player)
+        private static bool IsValid(Board board, VertexCoordinate vertex, IPlayer player)
         {
             ISettlement settlement = board[vertex];
             return settlement.Belongs is null || settlement.Belongs.Number == player.Number;
         }
 
-        public static bool IsValid(Board board, EdgeCoordinate edge, IPlayer player)
+        private static bool IsValid(Board board, EdgeCoordinate edge, IPlayer player)
         {
             IRoad road = board[edge];
             return road.Belongs is not null && road.Belongs.Number == player.Number;
         }
 
-        public static IEnumerable<EdgeCoordinate> RootRoads(Board board, IPlayer player)
+        private static IEnumerable<EdgeCoordinate> GetPlayersRoads(Board board, IPlayer player)
         {
-            IEnumerable<EdgeCoordinate> playersEdges = board.EdgeStore
+            return board.EdgeStore
                 .Select(entry => entry.Key)
                 .Where(edge => IsValid(board, edge, player));
-
-
-            return playersEdges.Where(edge =>
-            {
-                IEnumerable<VertexCoordinate> sharedVertices = edge.Neighbors()
-                    .SelectMany(neighbor => neighbor.Vertices())
-                    .Distinct()
-                    .Intersect(edge.Vertices());
-
-                return sharedVertices.Count() <= 1;
-            });
         }
 
-        public static VertexCoordinate? FindOutgoing(Board board, EdgeCoordinate edge, IPlayer player)
+        private static IEnumerable<EdgeCoordinate> GetRootRoads(IEnumerable<EdgeCoordinate> playersEdges, Board board, IPlayer player)
+        {
+            foreach (EdgeCoordinate edge in playersEdges)
+            {
+                try
+                {
+                    _ = FindOutgoing(board, edge, player);
+                }
+
+                // more than one outgoing side
+                catch (Exception)
+                {
+                    continue;
+                }
+
+                // none or exactly one outgoing side
+                yield return edge;
+            }
+        }
+
+        private static VertexCoordinate? FindOutgoing(Board board, EdgeCoordinate edge, IPlayer player)
         {
             IEnumerable<VertexCoordinate> outgoingVertices = edge.Vertices()
                 .Where(vertex => IsValid(board, vertex, player))
-                .Where(vertex => edge.Traverse(vertex)
-                    .Where(outgoingEdge => IsValid(board, outgoingEdge, player))
-                    .Any()
-                );
+                .Where(vertex => TraverseValid(board, edge, vertex, player).Any());
 
             return outgoingVertices.Count() switch
             {
@@ -53,20 +59,79 @@ namespace CatanLib.Helpers
             };
         }
 
-        public static VertexCoordinate? FindOutgoing(Board board, EdgeCoordinate edge, VertexCoordinate incommingVertex, IPlayer player)
+        private static IEnumerable<EdgeCoordinate> TraverseValid(Board board, EdgeCoordinate currentEdge, VertexCoordinate outgoingVertex, IPlayer player)
         {
-            VertexCoordinate outgoingVertex = edge.Other(incommingVertex);
+            return currentEdge.Traverse(outgoingVertex)
+                .Where(outgoingEdge => IsValid(board, outgoingEdge, player));
+        }
 
-            if (!IsValid(board, outgoingVertex, player))
+        private static int TraverseRootRoad(Board board, EdgeCoordinate root, IPlayer player, HashSet<EdgeCoordinate> totalRoads)
+        {
+            VertexCoordinate? outgoingVertex = FindOutgoing(board, root, player);
+
+            int max = 1;
+            if (outgoingVertex is null)
             {
-                return null;
+                _ = totalRoads.Remove(root);
+                return max;
             }
 
-            bool hasConnecting = edge.Traverse(outgoingVertex)
-                .Where(outgoingEdge => IsValid(board, outgoingEdge, player))
-                .Any();
+            TraverseRoad(board, root, outgoingVertex, player, max, ref max, new() { root.Other(outgoingVertex) }, totalRoads);
+            return max;
+        }
 
-            return hasConnecting ? outgoingVertex : null;
+        private static int TraverseCicularRoad(Board board, EdgeCoordinate edge, IPlayer player, HashSet<EdgeCoordinate> totalRoads)
+        {
+            int max = 1;
+            TraverseRoad(board, edge, edge.VertexA, player, max, ref max, new() { edge.VertexB }, totalRoads);
+            return max;
+        }
+
+        private static void TraverseRoad(Board board, EdgeCoordinate currentEdge, VertexCoordinate outgoingVertex, IPlayer player, int depth, ref int max, HashSet<VertexCoordinate> seenVetices, HashSet<EdgeCoordinate> totalRoads)
+        {
+            _ = totalRoads.Remove(currentEdge);
+            if (IsValid(board, outgoingVertex, player) && !seenVetices.Contains(outgoingVertex))
+            {
+                IEnumerable<EdgeCoordinate> validEdges = TraverseValid(board, currentEdge, outgoingVertex, player);
+                if (validEdges.Any())
+                {
+                    depth++;
+                    max = depth > max ? depth : max;
+
+                    foreach (EdgeCoordinate validEdge in validEdges)
+                    {
+                        TraverseRoad(board, validEdge, validEdge.Other(outgoingVertex), player, depth, ref max, new(seenVetices) { outgoingVertex }, totalRoads);
+                    }
+                }
+            }
+        }
+
+        public static void UpdateLongestRoad(ICatan catan)
+        {
+            Board board = catan.Board;
+            foreach (IPlayer player in catan.Players)
+            {
+                HashSet<EdgeCoordinate> roads = GetPlayersRoads(board, player).ToHashSet();
+                IEnumerable<EdgeCoordinate> roots = GetRootRoads(roads, board, player);
+
+                // travers all valid roots and single roads
+                IList<int> lengths = roots.Select(root => TraverseRootRoad(board, root, player, roads)).ToList();
+
+                // if roads are still left they must belong to a circular network
+                while (roads.Count > 0)
+                {
+                    lengths.Add(TraverseCicularRoad(board, roads.First(), player, roads));
+                }
+
+                player.LongestRoadLength = lengths.OrderByDescending(length => length).FirstOrDefault();
+            }
+
+            // secordary ordering by HasLongestRoad aligns players if tied for the longest road
+            IOrderedEnumerable<IPlayer> orderedPlayers = catan.Players.OrderByDescending(player => player.LongestRoadLength)
+                .ThenByDescending(player => player.HasLongestRoad);
+
+            _ = orderedPlayers.Skip(1).Select(player => player.HasLongestRoad = false);
+            orderedPlayers.First().HasLongestRoad = true;
         }
     }
 }
