b9a5e6c481ca77e4fb225d26de4f61638aff758f
3k-dome <doenerfresser3k@gmail.com>
2023-06-19 20:41:29 +0200

    add new vectorization interfaces

---
+++ catan-lib/Interfaces/Interaction/Vectorization/IVectorizableComponent.cs
@@ -0,0 +1,9 @@
+﻿using CatanLib.Interfaces.Components.Other;
+
+namespace CatanLib.Interfaces.Interaction.Vectorization;
+
+public interface IVectorizableComponent
+{
+    IEnumerable<float> ToVector(ICatan catan);
+    IEnumerable<string> ToDescriptiveVector(ICatan catan);
+}
