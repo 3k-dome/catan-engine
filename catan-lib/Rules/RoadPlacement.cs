3d999a5a395caa85a6349754fd24d4cf41775d5c
3k-dome <doenerfresser3k@gmail.com>
2023-06-21 23:27:28 +0200

    refactored some placement rules

---
+++ catan-lib/Rules/RoadPlacement.cs
@@ -0,0 +1,66 @@
+﻿using CatanLib.Interfaces.Components.Buildings;
+using CatanLib.Interfaces.Components.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatanLib.Rules;
+
+public static class RoadPlacement
+{
+    /// <summary>
+    /// A building spot is valid if it connects to a settlement owned by the player.
+    /// </summary>
+    private static bool AdjacentSettlement(IBoard board, IPlayer player, IRoad road)
+    {
+        return road.Edge.Vertices()
+            .Select(vertex => board[vertex])
+            .Where(settlement => settlement.Owner == player)
+            .Any();
+    }
+
+    /// <summary>
+    /// A building spot is valid if it connects to a road owned by the player and
+    /// the connection between those two is not iterrupted by another players settlement.
+    /// </summary>
+    private static bool AdjacentRoad(IBoard board, IPlayer player, IRoad road)
+    {
+        return road.Edge.Neighbors()
+            // select adjacent roads owned by the player
+            .Select(edge => board[edge])
+            .Where(adjacentRoad => adjacentRoad.Owner == player)
+
+            // select the settlement where the roads connect to each other
+            .Select(adjacentRoad => road.Edge.Vertices().Intersect(adjacentRoad.Edge.Vertices()).First())
+            .Select(vertex => board[vertex])
+
+            // see if the intersection settlement is unowned or owned by the player
+            .Where(settlement => settlement.Owner is null || settlement.Owner == player)
+            .Any();
+    }
+
+    /// <summary>
+    /// A building spot is valid if connects to a settlement owned by the player
+    /// or if it is part of an uninterrupted road.
+    /// </summary>
+    public static bool PlacementRule(IBoard board, IPlayer player, IRoad road)
+    {
+        return AdjacentSettlement(board, player, road) || AdjacentRoad(board, player, road);
+    }
+
+    /// <summary>
+    /// During the settlement phase two settlements and roads are placed each in a specific order.
+    /// A road is placed after the first settlment was built (exactly one settlement is placed) 
+    /// and again after the second settlment was built (exactly two settlements are placed).
+    /// </summary>
+    public static bool SettlementPhaseConstraint(IBoard board, IPlayer player)
+    {
+        int noSettlements = board.Settlements.Select(pair => pair.Value)
+            .Where(settlement => settlement.Owner == player)
+            .Count();
+
+        return noSettlements is 1 or 2;
+    }
+}
---
+++ catan-lib/Rules/SettlementPlacement.cs
@@ -0,0 +1,43 @@
+﻿using CatanLib.Interfaces.Components.Buildings;
+using CatanLib.Interfaces.Components.Other;
+
+namespace CatanLib.Rules;
+
+public static class SettlementPlacement
+{
+    /// <summary>
+    /// A building spot is valid if all surrounding building spots are empty.
+    /// </summary>
+    public static bool DistanceRule(IBoard board, ISettlement settlement)
+    {
+        return settlement.Coordinate.Neighbors()
+            .Where(vertex => board.Settlements.ContainsKey(vertex))
+            .Select(vertex => board[vertex])
+            .All(settlement => settlement.Owner is null);
+    }
+
+    /// <summary>
+    /// A building spot is valid if it connects to atleast one road owned by the player.
+    /// </summary>
+    public static bool PlacementRule(IBoard board, IPlayer player, ISettlement settlement)
+    {
+        return settlement.Coordinate.Edges()
+            .Where(edge => board.Roads.ContainsKey(edge))
+            .Select(edge => board[edge])
+            .Any(road => road.Owner == player);
+    }
+
+    /// <summary>
+    /// During the settlement phase two settlements and roads are placed each in a specific order.
+    /// A Settlement is placed first (no roads are placed yet) and again after the first road was
+    /// placed (exactly one road is placed).
+    /// </summary>
+    public static bool SettlementPhaseConstraint(IBoard board, IPlayer player)
+    {
+        int noRoads = board.Roads.Select(pair => pair.Value)
+            .Where(road => road.Owner == player)
+            .Count();
+
+        return noRoads is 0 or 1;
+    }
+}
