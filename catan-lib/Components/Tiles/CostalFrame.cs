5629ff2f29cdb573e931d4a852c3780c796f1d8c
3k-dome <doenerfresser3k@gmail.com>
2023-06-26 22:34:43 +0200

    add new costal tiles and frames

---
+++ catan-lib/Components/Tiles/CostalFrame.cs
@@ -0,0 +1,72 @@
+﻿using CatanLib.Enums;
+using CatanLib.Interfaces.Components.Tiles;
+using HexagonLib.Enums;
+
+namespace CatanLib.Components.Tiles;
+
+public class CostalFrame : ICostalFrame
+{
+    public ICostalTile[] Tiles { get; private set; }
+
+    private CostalFrame(ICostalTile[] tiles)
+    {
+        if (tiles.Length != 3)
+        {
+            throw new ArgumentException("A frame must consist of exactly 3 tiles!");
+        }
+
+        Tiles = tiles;
+    }
+
+    public static IEnumerable<ICostalFrame> Frames { get; } = new List<ICostalFrame>()
+    {
+        // 2, 1
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, null, true),
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.SouthWest, TileVertexDirection.South }, Resource.Sheep, false)
+
+        }),
+
+        // 3, 2
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, Resource.Ore, false),
+            new CostalTile(null, null, false),
+        }),
+
+        // 4, 3
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, null, true),
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.SouthWest, TileVertexDirection.South }, Resource.Wheat, false)
+        }),
+
+        // 5, 3
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, Resource.Wood, false),
+            new CostalTile(null, null, false),
+        }),
+
+        // 6, 5
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, null, true),
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.SouthWest, TileVertexDirection.South }, Resource.Brick, false)
+        }),
+
+        // 1, 6
+        new CostalFrame(new CostalTile[]
+        {
+            new CostalTile(null, null, false),
+            new CostalTile(new[] { TileVertexDirection.South, TileVertexDirection.SouthEast }, null, true),
+            new CostalTile(null, null, false),
+        }),
+    };
+}
---
+++ catan-lib/Components/Tiles/CostalTile.cs
@@ -0,0 +1,74 @@
+﻿using CatanLib.Encoders;
+using CatanLib.Enums;
+using CatanLib.Interfaces.Components.Other;
+using CatanLib.Interfaces.Components.Tiles;
+using HexagonLib;
+using HexagonLib.Enums;
+
+namespace CatanLib.Components.Tiles;
+
+public class CostalTile : ICostalTile
+{
+    private readonly IEnumerable<TileVertexDirection>? defaultAccessibleFrom;
+    public IEnumerable<TileVertexDirection>? AccessibleFrom { get; private set; }
+    public Resource? TradesResource { get; private set; }
+    public bool TradesAny { get; private set; }
+
+    private TileCoordinate? coordinate;
+    public TileCoordinate Coordinate
+    {
+        get => coordinate ?? throw new NullReferenceException("Coordinate not set.");
+        set { coordinate = value; AlignAccess(); }
+    }
+
+    public CostalTile(IEnumerable<TileVertexDirection>? accessibleFrom, Resource? resource, bool any)
+    {
+        if (accessibleFrom is not null)
+        {
+            if (accessibleFrom.Count() != 2)
+            {
+                throw new ArgumentException("Harbors may only be accessed from to directions!");
+            }
+
+            if (accessibleFrom.Any() && resource is null && !any)
+            {
+                throw new ArgumentException("A harbory must provide trades if it is accessibe!");
+            }
+        }
+
+        if (resource is not null && any)
+        {
+            throw new ArgumentException("Harbors may only trade any resource or one specific at the time!");
+        }
+
+        (TradesResource, TradesAny, defaultAccessibleFrom) = (resource, any, accessibleFrom);
+    }
+
+    private void AlignAccess()
+    {
+        if (defaultAccessibleFrom is not null)
+        {
+            TileVertexDirection alignment = Coordinate.GetAlignment();
+            AccessibleFrom = defaultAccessibleFrom.Select(direction => (int)direction + (int)alignment)
+                .Select(direction => direction % (TileVertex.Directions.Count() - 1))
+                .Cast<TileVertexDirection>()
+                .ToArray();
+        }
+        else
+        {
+            AccessibleFrom = null;
+        }
+    }
+
+    public IEnumerable<float> ToVector(ICatan catan)
+    {
+        IEnumerable<float> encoding = ResourceEncoder.EncodeResource(TradesResource);
+        return encoding.Append(TradesAny ? 1f : 0f);
+    }
+
+    public IEnumerable<string> ToDescriptiveVector(ICatan catan)
+    {
+        IEnumerable<string> encoding = ResourceEncoder.EncodeResourceDescriptive();
+        return encoding.Append("Trades any 3:1");
+    }
+}
