384ed1af04b48e1b71d5e3dfbd221b57d8695b17
3k-dome <doenerfresser3k@gmail.com>
2023-09-24 20:06:10 +0200

    renamed gui project

---
+++ catan-engine.gui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CatanEngine.Gui;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs eventArgs)
+    {
+        base.OnStartup(eventArgs);
+    }
+}
